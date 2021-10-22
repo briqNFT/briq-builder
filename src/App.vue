e18d8eb2b408fbaee508e22ff4b2c32da76809f5
s0lness <sylve.chevet1@gmail.com>
2021-10-23 00:29:01 +0200

    there we go

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
 </script>
 
 <template>
   <h1>brq</h1>
   <h2>Seize the brqs of construction</h2>
-    <HelloWorld/>
+    <Homepage/>
 </template>
 
-<script lang="ts">
-import { reactive } from 'vue'
-
-var contract = reactive({
-	connected: false,
-});
-
-setTimeout(() => { contract.connected = true; }, 5000);
-
-import { defineComponent } from 'vue';
-export default defineComponent({
-	provide: {
-		contract
-	}
-});
-</script>
-
 <style>
+@import url('http://fonts.cdnfonts.com/css/common-pixel');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
