40e306bfb8abc6ab0072c1ddd37b15ba2187bfa0
s0lness <sylve.chevet1@gmail.com>
2021-10-22 23:06:03 +0200

    put the buidler

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>brq</h1>
+  <h2>Seize the brqs of construction</h2>
+    <HelloWorld/>
 </template>
+
+<script lang="ts">
+import { reactive } from 'vue'
+
+var contract = reactive({
+	connected: false,
+});
+
+setTimeout(() => { contract.connected = true; }, 5000);
+
+import { defineComponent } from 'vue';
+export default defineComponent({
+	provide: {
+		contract
+	}
+});
+</script>
 
 <style>
 #app {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <canvas id="backgroundgl" ref="canvas">
+    </canvas>
 </template>
 
+<script lang="ts">
+    import { main } from "../builder.js"
+
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        async mounted() {
+            main(this.$refs.canvas);
+    ;    },
+        inject: ["contract"]
+    });
+</script>
+
 <style scoped>
-a {
-  color: #42b983;
+
+html, body {
+  height: 100%;
+  margin: 0;
 }
+
+#backgroundgl {
+  width: 95%;
+  height: 100%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 15px;
+}
+
 </style>
