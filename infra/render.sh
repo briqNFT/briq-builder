c273f73a069daa0d659b07679ad9ea8162057b3e
Lancelot de Ferrière <wraitii@gmail.com>
2021-11-01 13:50:26 +0100

    Infra update, rename branch to main

--- infra/render.sh
+++ infra/render.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+TAG=${TAG:=$(git rev-parse HEAD)}
+
 mkdir -p infra/dist
 ytt -f infra/schema.yaml -f infra/ --data-value "image_tag=$TAG" > infra/dist/manifests.yaml
 echo "validating client-side"
--- infra/run-docker.sh
+++ infra/run-docker.sh
@@ -3,13 +3,14 @@ set -e
 
 npx vite build --outDir infra/dist/www/
 
+NAME="briq-builder"
 TAG=${TAG:=$(git rev-parse HEAD)}
 echo "Building docker image, tagging $TAG"
-IMAGE_NAME="europe-west3-docker.pkg.dev/healthy-saga-329513/sltech-briq/briq-builder:$TAG"
+IMAGE_NAME="europe-west3-docker.pkg.dev/healthy-saga-329513/sltech-briq/$NAME:$TAG"
 
 docker build infra/ -t "$IMAGE_NAME"
 docker push "$IMAGE_NAME"
 # Tag the image as 'latest' locally
-docker tag "$IMAGE_NAME" briq-builder:latest
+docker tag "$IMAGE_NAME" "$NAME:latest"
 # Cleanup
 docker image rm "$IMAGE_NAME"
