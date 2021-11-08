70645c8fd6a19b2fc31b2aea51c5ca1f51b15168
Lancelot de Ferrière <wraitii@gmail>
2021-11-08 18:01:36 +0100

    Suppport briq.construction
    Update ingress & URL schemes.

--- src/url.js
+++ src/url.js
@@ -3,6 +3,6 @@ export default function getBaseUrl()
 {
     var base_url = "http://localhost:5000"
     if (import.meta.env.PROD)
-        base_url = "https://api.sltech.company"
+        base_url = "https://api." + window.location.hostname
     return base_url
 }
