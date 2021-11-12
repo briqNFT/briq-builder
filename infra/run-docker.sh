68788d91e2251875d3763d1bb24ad78a405c5e62
Lancelot de Ferrière <wraitii@gmail.com>
2021-11-12 10:49:51 +0100

    Explicit docker target

--- infra/run-docker.sh
+++ infra/run-docker.sh
@@ -8,7 +8,7 @@ TAG=${TAG:=$(git rev-parse HEAD)}
 echo "Building docker image, tagging $TAG"
 IMAGE_NAME="europe-west3-docker.pkg.dev/healthy-saga-329513/sltech-briq/$NAME:$TAG"
 
-docker build infra/ -t "$IMAGE_NAME"
+docker build --platform linux/amd64 infra/ -t "$IMAGE_NAME"
 docker push "$IMAGE_NAME"
 # Tag the image as 'latest' locally
 docker tag "$IMAGE_NAME" "$NAME:latest"
