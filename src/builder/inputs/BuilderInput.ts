df83764e415bc5f6b824bb21e7eb44587e7e5df5
Lancelot de Ferrière <wraitii@gmail.com>
2021-11-30 17:42:36 +0100

    Eraser multi-input

--- src/builder/inputs/BuilderInput.ts
+++ src/builder/inputs/BuilderInput.ts
@@ -1,4 +1,7 @@
+import { number } from 'starknet';
 import { reactive } from 'vue';
+
+import { getCameraRay, voxWorld } from "../graphics/builder.js"
 
 export class BuilderInputState
 {
@@ -14,6 +17,64 @@ export class BuilderInputState
     onPointerDown(event: PointerEvent) {}
     onPointerUp(event: PointerEvent) {}
 }
+
+
+export class MouseInputState extends BuilderInputState
+{
+    curX: number = 0;
+    curY: number = 0;
+    lastX: number = 0;
+    lastY: number = 0;
+
+    lastClickX: number = 0;
+    lastClickY: number = 0;
+
+    canvas: HTMLCanvasElement;
+
+    constructor(canvas: HTMLCanvasElement, guiState: any = {}) {
+        super(guiState);
+        this.canvas = canvas;
+    }
+
+    onEnter() {}
+    onExit() {}
+
+    getCanvasRelativePosition(x: number, y: number): [number, number]
+    {
+        const rect = this.canvas.getBoundingClientRect();
+        return [
+            (this.curX - rect.left) / rect.width * this.canvas.width / rect.width,
+            (this.curY - rect.top) / rect.height * this.canvas.height / rect.height
+        ]
+    }
+
+    getIntersectionPos(x: number, y: number, overlay = false): [number, number, number] | undefined
+    {
+        let [start, end] = getCameraRay(...this.getCanvasRelativePosition(x, y));
+        const intersection = voxWorld.intersectRay(start, end);
+        if (!intersection)
+            return undefined;
+        return intersection.position.map((v, ndx) => {
+            return Math.floor(v + intersection.normal[ndx] * (overlay ? -0.5 : +0.5));
+        });
+    }
+
+    onPointerMove(event: PointerEvent) {
+        this.lastX = this.curX;
+        this.lastY = this.curY;
+        this.curX = event.clientX;
+        this.curY = event.clientY;
+    }
+
+    onPointerDown(event: PointerEvent) {
+        this.lastClickX = event.clientX;
+        this.lastClickY = event.clientY;
+    }
+
+    onPointerUp(event: PointerEvent) {
+    }
+}
+
 
 class NullState extends BuilderInputState
 {
