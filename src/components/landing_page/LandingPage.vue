a3761b18d3a2d6248a59da30a3371aaf993c9175
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-02 14:19:53 +0100

    Install FontAwesome, responsivize

--- src/components/landing_page/LandingPage.vue
+++ src/components/landing_page/LandingPage.vue
@@ -23,37 +23,37 @@ import StarkwareIcon from './starkware.png'
             </div>
         </div>
         <div class="row">
-            <div class="flex justify-around flex-nowrap my-10">
+            <div class="flex flex-wrap justify-around my-10">
                 <div class="centerall flex-1">
-                    <h1 class="font-display font-black">briq</h1>
-                    <p>NFT matter.<br/>NFTs that matter.<br/>Seize the briqs of construction.</p>
+                    <h1 class="text-8xl font-display font-black">briq</h1>
+                    <p class="my-8 text-2xl">NFT matter.<br/>NFTs that matter.<br/>Seize the briqs of construction.</p>
                     <button>Build</button>
                 </div>
-                <div class="centerall flex-1"><GMIcon/></div>
+                <div class="centerall flex-1"><GMIcon class="lg:scale-100 scale-75"/></div>
             </div>
         </div>
         <div class="bg-briq-dark my-20 row">
-            <div class="flex justify-between flex-nowrap">
-                <div class="centerleft flex-auto"><ComposableIcon/></div>
-                <div class="centerright flex-initial">
+            <div class="flex flex-wrap justify-between">
+                <div class="md:centerleft centerall flex-auto"><ComposableIcon/></div>
+                <div class="md:centerright centerall flex-initial">
                     <h2>NFTS should be composable</h2>
                     <p>SOEhtosejr ozjetoizjefojzeof oziefjosidjf oisdjf ozijsdfoizjfo izjoifj soidjf osdjf osdjfo  sdofisdoifj </p>
                 </div>
             </div>
         </div>
         <div class="row bg-briq-dark my-20">
-            <div class="flex justify-between flex-nowrap">
-                <div class="centerleft flex-initial">
+            <div class="flex flex-wrap justify-between">
+                <div class="centerall md:centerleft flex-initial">
                     <h2>NFTS should be interoperable</h2>
                     <p>SOEhtosejr ozjetoizjefojzeof oziefjosidjf oisdjf ozijsdfoizjfo izjoifj soidjf osdjf osdjfo  sdofisdoifj </p>
                 </div>
-                <div class="centerright flex-auto"><InteroperableIcon/></div>
+                <div class="centerall md:centerright flex-auto"><InteroperableIcon/></div>
             </div>
         </div>
         <div class="row bg-briq-dark my-20">
-            <div class="flex justify-between flex-nowrap">
-                <div class="centerleft flex-auto"><OnChainIcon/></div>
-                <div class="centerright flex-initial">
+            <div class="flex flex-wrap justify-between">
+                <div class="centerall md:centerleft flex-auto"><OnChainIcon/></div>
+                <div class="centerall md:centerright flex-initial">
                     <h2>NFTS should be On-Chain</h2>
                     <p>SOEhtosejr ozjetoizjefojzeof oziefjosidjf oisdjf ozijsdfoizjfo izjoifj soidjf osdjf osdjfo  sdofisdoifj </p>
                 </div>
@@ -61,27 +61,29 @@ import StarkwareIcon from './starkware.png'
         </div>
         <div class="my-40"></div>
         <div class="row py-8">
-            <div class="flex justify-around">
-                <div class="flex-1">
-                    <h5>Backed By</h5>
-                    <div class="flex justify-around">
+            <div class="flex justify-around flex-wrap">
+                <div class="flex-initial">
+                    <h2>BACKED BY</h2>
+                    <div class="flex justify-around flex-wrap">
                         <img class="px-20 py-10" :src="FabricIcon"/>
                         <img class="px-20 py-10" :src="FirstIcon"/>
                     </div>
                 </div>
-                <div class="bg-white h-auto" style="width:1px;"></div>
-                <div class="flex-1">
-                    <h5>Partners</h5>
-                    <img class="px-20 py-10" :src="StarkwareIcon"/>
+                <div class="bg-white h-auto lg:block hidden" style="width:2px;"></div>
+                <div class="flex-initial">
+                    <h2>PARTNERS</h2>
+                    <div class="flex justify-around">
+                        <img class="px-20 py-10" :src="StarkwareIcon"/>
+                    </div>
                 </div>
             </div>
-            <div class="flex justify-center gap-10 !my-10">
-                <p>Twitter</p>
-                <p>Discord</p>
-                <p>Github</p>
+            <div class="flex justify-center gap-10 !my-10 text-5xl">
+                <p><i class="fab fa-twitter"></i></p>
+                <p><i class="fab fa-discord"></i></p>
+                <p><i class="fab fa-github"></i></p>
             </div>
             <div>
-                <p>Copyright © 2021 S&L Technologies. All rights reserved. Legal Terms</p>
+                <p class="font-light">Copyright © 2021 S&L Technologies. All rights reserved. <router-link class="font-medium px-4" to="/legal">Legal terms</router-link></p>
             </div>
         </div>
     </div>
@@ -100,6 +102,9 @@ export default defineComponent({
 .header {
     @apply container m-auto lg:w-[70rem];
 }
+.row {
+    @apply py-20;
+}
 .row > div {
     @apply container m-auto lg:w-[70rem];
 }
@@ -112,6 +117,11 @@ export default defineComponent({
 .centerleft {
     @apply flex flex-col flex-nowrap justify-center items-start text-left;
 }
+
+h2 {
+    @apply font-black my-4;
+}
+
 svg {
     @apply drop-shadow-xl;
 }
