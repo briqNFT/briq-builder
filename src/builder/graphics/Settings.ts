14ce284245cebbacba382a3a90521598efada1ec
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-07 18:02:27 +0100

    Persist settings

--- src/builder/graphics/Settings.ts
+++ src/builder/graphics/Settings.ts
@@ -1,6 +1,19 @@
-import { reactive } from 'vue';
+import { reactive, watchEffect } from 'vue';
 
-export default reactive({
+const store = reactive({
     useSAO: true,
     useRealAA: true,
+});
+
+for (let key in store)
+{
+    let val = window.localStorage.getItem("setting_" + key);
+    if (val)
+        store[key] = JSON.parse(val);
+}
+export default store;
+
+watchEffect(() => {
+    for (let key in store)
+        window.localStorage.setItem("setting_" + key, JSON.stringify(store[key]));
 });
