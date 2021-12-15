5655357e76c3bc7896366f736d1907dbea9e3e9f
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-15 19:15:20 +0100

    Update for testing env

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,6 +5,8 @@ set -e
 #exit 0
 
 export TAG=${TAG:=$(git rev-parse HEAD)}
+export K8S_ENV=${K8S_ENV:="test"}
+
 infra/run-docker.sh
 infra/render.sh
-kubectl apply -f infra/dist/manifests.yaml --validate=true
+kubectl apply -f infra/dist/manifests-${K8S_ENV}.yaml --validate=true
