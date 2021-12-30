ab68b2ddaab8b16e7258d8b8ca9fe0a5e1250cab
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-30 10:54:52 +0100

    update config, add sourcemap

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,29 @@ import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
 svgLoader({
-  svgoConfig: {
-    multipass: true
-  }
+    svgoConfig: {
+        multipass: true
+    }
 })
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    target: "es2020",
-  },
-  plugins: [vue(), svgLoader()]
-})
+export default defineConfig(({ mode }) => ({
+    build: {
+        target: "es2020",
+        sourcemap: mode === "PROD",
+        /*
+        minify: false,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    threejs: ['three'],
+                    sentry: ['@sentry/vue', '@sentry/tracing'],
+                    vendor: ['starknet'],
+                    vue: ['vue', 'vuex', 'vue-router']
+                }
+            }
+        }
+        */
+    },
+    plugins: [vue(), svgLoader()]
+}));
