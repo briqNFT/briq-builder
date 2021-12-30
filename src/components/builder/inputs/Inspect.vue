0d97808146e7fcb00f63469adde1728e606b40e9
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-30 10:39:00 +0100

    show temp briqs as temporary

--- src/components/builder/inputs/Inspect.vue
+++ src/components/builder/inputs/Inspect.vue
@@ -7,8 +7,8 @@ import BriqSwapModal from '../modals/BriqSwapModal.vue';
     <div class="alternate-buttons">
         <template v-if="selection.selectedBriqs.length <= 2">
             <div  class="bg-briq rounded-md px-2 py-1" v-for="briqId of selection.selectedBriqs">
-                <p class="text-sm tracking-tighter break-all">{{ selection.set?.briqsDB.get(briqId).id }}</p>
-                <Button @click="openSwapModal(briqId)">Swap briq</Button>
+                <p class="text-sm tracking-tighter break-all">{{ getBriqIdentifier(selection.set?.briqsDB.get(briqId).id) }}</p>
+                <Button :disabled="$store.state.builderData.briqsDB.briqs.size === 0" @click="openSwapModal(briqId)">Swap briq</Button>
             </div>
         </template>
         <template v-else="">
@@ -18,7 +18,7 @@ import BriqSwapModal from '../modals/BriqSwapModal.vue';
     <!-- Follows the mouse -->
     <div v-if="fsm?.briq" class="fixed pointer-events-none" :style="{ 'left': `${fsm.curX+20}px`, 'top': `${fsm.curY+20}px` }">
         <div class="w-auto min-w-32 h-32 bg-briq p-2 rounded-md shadow-md">
-        <h4>Briq: <span class="tracking-tighter">{{ fsm?.briq?.id ?? '' }}</span></h4>
+        <h4>Briq: <span class="tracking-tighter">{{ getBriqIdentifier(fsm?.briq?.id ?? '') }}</span></h4>
         <p>Color: {{ fsm.briq.color }}</p>
         <p>Material: {{ mapMat(fsm.briq.material) }}</p>
         </div>
@@ -49,6 +49,11 @@ export default defineComponent({
             if (choice?.briq?.id)
                 await this.$store.dispatch("builderData/swap_briqs", [[briqId, choice.briq.id]]);
             setModal();
+        },
+        getBriqIdentifier(id: string) {
+            if (this.$store.state.builderData.briqsDB.briqs.has(id))
+                return id;
+            return "Temporary briq";
         }
     },
 })
