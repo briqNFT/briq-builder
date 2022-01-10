c0e9d6d7843e5b46b8bef4e3362e16c116179874
Lancelot de Ferrière <wraitii@gmail.com>
2022-01-10 18:47:07 +0100

    Fix UUIDs starting with 0s

--- src/Uuid.ts
+++ src/Uuid.ts
@@ -2,9 +2,15 @@ import { v4 as uuidv4 } from 'uuid';
 
 export { uuidv4 };
 
-export function hexUuid(): string
+export function hexUuid(input?: Array<number>): string
 {
     let buf = [] as Array<number>;
-    uuidv4(undefined, buf);
-    return "0x" + buf.map(x => x.toString(16).padStart(2, '0')).join("");
+    if (input)
+        buf = input;
+    else
+        uuidv4(undefined, buf);
+    let res = buf.map(x => x.toString(16).padStart(2, '0')).join("");
+    if (res[0] === "0")
+        res = res.replace(/^0+/, '');
+    return "0x" + res;
 }
