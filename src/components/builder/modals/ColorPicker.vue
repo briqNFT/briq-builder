59adb613ed7614ff77784ff385ec6107c74c8de6
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-01 17:15:50 +0100

    Switch color picker to Iro.js

--- src/components/builder/modals/ColorPicker.vue
+++ src/components/builder/modals/ColorPicker.vue
@@ -2,19 +2,13 @@
 </script>
 
 <template>
-    <div class="md:w-2/5 w-auto">
+    <div class="md:w-3/5 w-auto">
         <div class="relative">
             <button @click="$emit('close')" class="absolute right-0">X</button>
             <h2 class="text-center">Color Picker</h2>
-            <div class="flex justify-stretch flex-col w-min font-medium">
-                <p class="inline-block flex justify-between">Hue:
-                    <input class="mx-2" v-model="hue" type="range" min="0" max="3600"/></p>
-                <p class="inline-block flex justify-between">Saturation:
-                    <input class="mx-2" v-model="saturation" type="range" min="0" max="1000"/></p>
-                <p class="inline-block flex justify-between">Luminance:
-                    <input class="mx-2" v-model="luminance" type="range" min="0" max="1000"/></p>
-                <p class="inline-block flex my-2"><div class="h-8 w-16 rounded-md" :style="{ 'backgroundColor': getHSL()}"></div>
-                    <input class="mx-2" v-model="name" type="text" maxlength="7"/></p>
+            <div class="flex justify-stretch items-center flex-col w-full font-medium pt-8">
+                <ColorPicker :color="color || metadata?.color" @colorChange="(col) => { color = col; }"/>
+                <input type="text" maxlength="7" size="7" v-model="color" class="my-4">
             </div>
             <Btn class="float-right" @click="pickColor">Pick</Btn>
         </div>
@@ -22,62 +16,28 @@
 </template>
 
 <script lang="ts">
-import { THREE } from '../../../three';
-
-import { inputStore } from '../../../builder/inputs/InputStore'
+import ColorPicker from '../../generic/ColorPicker.vue';
 
 import { defineComponent } from 'vue';
 export default defineComponent({
     data() {
         return {
-            hue: 0,
-            saturation: 700,
-            luminance: 700,
-            customName: ""
+            color: undefined as undefined | string
         }
     },
+    components: { ColorPicker },
     props: ["metadata"],
     mounted() {
         if (!(this.metadata?.color))
             return;
-        this.parse(this.metadata.color);
+        this.color = this.metadata?.color;
     },
     methods: {
-        parse(colorHex: string)
+        pickColor()
         {
-            let col: THREE.HSL = { h: 0, s: 0, l: 0 };
-            let c = new THREE.Color(colorHex);
-            c.getHSL(col);
-            this.hue = Math.round(col.h * 3600.0);
-            this.saturation = Math.round(col.s * 1000);
-            this.luminance = Math.round(col.l * 1000);
-        },
-        getHSL: function(round = false)
-        {
-            return `hsl(${Math.round(this.hue/10.0)}, ${Math.round(this.saturation/10.0)}%, ${Math.round(this.luminance/10.0)}%)`;
-        },
-        getHex() {
-            return "#" + new THREE.Color(0).offsetHSL(this.hue/3600.0, this.saturation/1000.0, this.luminance / 1000.0).getHexString();
-        },
-        pickColor: function() {
-            let hex = this.getHex();
-            this.$emit('close', [hex, hex]);
+            this.$emit('close', [this.color, this.color]);
         }
     },
     computed: {
-        name: {
-            get() {
-                return this.customName ? this.customName : this.getHex();
-            },
-            set(x) {
-                if (x.match(/^\#[abcdef0-9]{6}$/i))
-                {
-                    this.parse(x);
-                    this.customName = "";
-                }
-                else
-                    this.customName = x;
-            },
-        },
     }
 })</script>
