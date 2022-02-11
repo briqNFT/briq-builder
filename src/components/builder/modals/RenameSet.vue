0f8f3569e5cdc179c54744846d9ed5f4e8076eac
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-11 15:40:31 +0100

    Fixes to renaming modal

--- src/components/builder/modals/RenameSet.vue
+++ src/components/builder/modals/RenameSet.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="">
+    <div class="w-auto xl:max-w-[49%] lg:max-w-[80%] max-w-full">
         <div class="relative">
             <button @click="$emit('close')" class="absolute right-0">X</button>
             <h3 class="text center w-full">Rename set</h3>
             <p>Set {{ metadata.set }}</p>
             <p class="break-all">Current name: {{ set.name }}</p>
-            <p>New name: <input v-model="name" type="text" maxlength="200" minlength="1" size="60"/></p>
+            <p class="md:block hidden">New name: <input v-model="name" type="text" maxlength="200" minlength="1" size="60"/></p>
+            <p class="md:hidden block">New name: <input v-model="name" type="text" maxlength="200" minlength="1" size="30"/></p>
             <button class="btn float-right" @click="save">Save</button>
         </div>
     </div>
