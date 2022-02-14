77aff7e94eddf02313819663cdf5a6d2748e2721
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-14 16:54:58 +0100

    Add https:// to sharing link

--- src/components/builder/Sharing.ts
+++ src/components/builder/Sharing.ts
@@ -3,7 +3,7 @@ import type { SetData } from "../../builder/SetData";
 export const SHARE_VERSION = 2;
 
 export function getShareLink(network: string, setId: string, version: number = SHARE_VERSION) {
-    return `${window.location.hostname}/share?set_id=${setId}&network=${network}&version=${version}`;
+    return `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : '' }/share?set_id=${setId}&network=${network}&version=${version}`;
 };
 
 export function getRelativeShareLink(network: string, setId: string, version: number = SHARE_VERSION) {
