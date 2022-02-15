58993e3db47f0fd1fce670e5ac02a0152ad8b7d1
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-15 10:35:17 +0100

    Allow setting implementation in admin page

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -14,6 +14,14 @@
             <label><p><input v-model="qty" type="number"/> quantity</p></label>
             <Btn @click="mint(address, qty)">Mint</Btn>
         </div>
+        <div>
+            <h3>Contract initialisation</h3>
+            <p class="font-mono">Set proxy : {{ contractStore.set?.connectedTo }} -> {{ getSetImpl }}</p>
+            <p><input v-model="newSetImpl" type="text"> <Btn @click="setImpl(contractStore.set, newSetImpl)">Set new implementation</Btn></p>
+            <p class="font-mono">Briq proxy: {{ contractStore.briq?.connectedTo }} -> {{ getBriqImpl }}</p>
+            <p><input v-model="newBriqImpl" type="text"> <Btn @click="setImpl(contractStore.briq, newBriqImpl)">Set new implementation</Btn></p>
+
+        </div>
     </div>
 </template>
 
@@ -21,12 +29,20 @@
 import contractStore from '../Contracts';
 import { messagesStore, pushMessage } from '../Messages'
 import { defineComponent } from 'vue';
+import type { Provider, Signer } from 'starknet';
+import { getSelectorFromName } from 'starknet/utils/stark';
 
 export default defineComponent({
     data() {
         return {
             address: "",
             qty: 0,
+
+            _setImpl: "",
+            _briqImpl: "",
+
+            newSetImpl: "",
+            newBriqImpl: "",
         }
     },
     computed: {
@@ -35,7 +51,25 @@ export default defineComponent({
         },
         messages() {
             return messagesStore.messages;
-        }
+        },
+        getSetImpl() {
+            if (contractStore?.set?.connectedTo)
+                (this.$store.state.wallet.provider as Provider).callContract({
+                    contract_address: contractStore.set.connectedTo,
+                    entry_point_selector: getSelectorFromName("getImplementation"),
+                    calldata: []
+                }).then(rep => this._setImpl = rep.result[0]);
+            return this._setImpl;
+        },
+        getBriqImpl() {
+            if (contractStore?.briq?.connectedTo)
+                (this.$store.state.wallet.provider as Provider).callContract({
+                    contract_address: contractStore.briq.connectedTo,
+                    entry_point_selector: getSelectorFromName("getImplementation"),
+                    calldata: []
+                }).then(rep => this._briqImpl = rep.result[0]);
+            return this._briqImpl;
+        },
     },
     methods: {
         pushMessage,
@@ -46,6 +80,14 @@ export default defineComponent({
                 return;
             }
             pushMessage((await contractStore.briq?.mint(address, qty))?.toString() ?? "Failed to mint, contract is unset");
+        },
+        async setImpl(contract: any, address: string) {
+            let tx = await (this.$store.state.wallet.signer as Signer).invokeFunction(
+                contract.connectedTo,
+                getSelectorFromName("setImplementation"),
+                [address]
+            );
+            pushMessage("" + tx);
         }
     }
 })
