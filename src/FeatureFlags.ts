1b63c32bf1629c68ad4c384cb7faca61c109782e
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-21 11:24:32 +0100

    Activate movement feature for all

--- src/FeatureFlags.ts
+++ src/FeatureFlags.ts
@@ -4,7 +4,7 @@ import { reactive, watchEffect } from 'vue';
 import { logDebug } from "./Messages";
 
 export const featureFlags = reactive({
-    briq_select_movement: false,
+    briq_select_movement: true,
 });
 
 async function checkOnStore() {
@@ -14,9 +14,9 @@ async function checkOnStore() {
     watchEffect(() => {
         if (store?.store?.state?.wallet?.userWalletAddress == "0x3e46c8abcd73a10cb59c249592a30c489eeab55f76b3496fd9e0250825afe03" ||
             store?.store?.state?.wallet?.userWalletAddress == "0x6043ed114a9a1987fe65b100d0da46fe71b2470e7e5ff8bf91be5346f5e5e3")
-            featureFlags.briq_select_movement = true;
-        else
-            featureFlags.briq_select_movement = false;
+        {
+            // Admin-only.
+        }
     })
 };
 checkOnStore();
