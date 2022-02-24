6726d6ff7d11e10f47286b75eab0ac8833b9e73f
Lancelot de Ferrière <wraitii@gmail.com>
2022-02-24 16:09:29 +0100

    Download modal

---
+++ src/components/builder/modals/DownloadSet.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="w-auto">
+        <div class="relative">
+            <button @click="$emit('close')" class="absolute right-0">X</button>
+            <h2 class="text-center">Download set</h2>
+            <div class="w-full">
+                <div class="sm:w-max m-auto flex items-stretch flex-col font-medium gap-6 py-4 px-2">
+                    <div class="text-center">
+                        <Btn @click="downloadBriq">Download as .json</Btn>
+                        <p class="text-sm">This format is specific to briq and can be used to import the set.</p>
+                    </div>
+                    <div class="text-center">
+                        <Btn @click="downloadGLB">Download as .glb</Btn>
+                        <p class="text-center text-sm">GLB is binary GTLF, a modern open standard for 3D meshes.<br/>You can use this to import the set in other 3D worlds.</p>
+                    </div>
+                    <div class="text-center">
+                        <Btn @click="downloadVox">Download as .vox</Btn>
+                        <p class="text-center text-sm">Vox is the format used by MagicaVoxel and supported by most voxel editors.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import getBaseUrl, { doDownload } from '../../../url';
+export default defineComponent({
+    data() {
+        return {
+        }
+    },
+    props: ["metadata"],
+    mounted() {
+    },
+    computed: {
+        setId() {
+            return this.metadata.setId;
+        }
+    },
+    methods: {
+        async downloadBriq() {
+            doDownload(`${getBaseUrl()}/store_get/${this.setId}`, `${this.setId}.json`);
+        },
+        async downloadGLB() {
+            doDownload(`${getBaseUrl()}/get_model/${this.setId}.glb`, `${this.setId}.glb`);
+        },
+        async downloadVox() {
+            doDownload(`${getBaseUrl()}/get_model/${this.setId}.vox`, `${this.setId}.vox`);
+        },
+    },
+})
+</script>
