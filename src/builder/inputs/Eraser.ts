1d2f08c75fb2b6c9352af9bb51b5d47aacda27a2
Lancelot de Ferrière <wraitii@gmail.com>
2022-03-03 11:43:40 +0100

    Standardize voxel-aligned selection using a generic class

--- src/builder/inputs/Eraser.ts
+++ src/builder/inputs/Eraser.ts
@@ -1,6 +1,7 @@
 import { MouseInputState } from './BuilderInputState';
+import { VoxelAlignedSelection } from './Selecting';
 import getPreviewCube from '../graphics/PreviewCube'
-import { store } from '../../store/Store'
+import { store } from '../../store/Store';
 
 import { THREE } from '../../three';
 
@@ -35,7 +36,7 @@ export class EraserInput extends MouseInputState
         }
     }
 
-    onPointerDown(event: PointerEvent)
+    async onPointerDown(event: PointerEvent)
     {
         if (event.shiftKey)
             this.fsm.switchTo("erase_multi", { x: event.clientX, y: event.clientY });
@@ -58,66 +59,22 @@ export class EraserInput extends MouseInputState
     }
 }
 
-export class EraserMultiInput extends MouseInputState
+export class EraserMultiInput extends VoxelAlignedSelection
 {
-    lastClickPos!: [number, number, number];
-
     onEnter(data: any) {
-        this.curX = data.x;
-        this.curY = data.y;
-
-        this.lastClickPos = this.getIntersectionPos(this.curX, this.curY, true)!;
-        if (!this.lastClickPos)
-            throw new Error("Error: EraserMultiInput must have a well defined event position on entry");
-        
-        this.fsm.orbitControls.enabled = false;
-        getPreviewCube().visible = true;
+        this.shouldClampToBounds = false;
+        super.onEnter(data);
+        this.switchBackTo = "erase";
         (getPreviewCube().material as THREE.MeshPhongMaterial).color = new THREE.Color(0x962400);
     }
 
-    onExit() {
-        getPreviewCube().visible = false;
-        this.fsm.orbitControls.enabled = true;
-    }
-
-    onPointerMove(event: PointerEvent)
-    {
-        let pos = this.getIntersectionPos(this.curX, this.curY, true);
-        if (!pos)
-            return;
-
-        if (this.lastClickPos[1] === -1 && pos[1] === -1)
-            pos[1] = 0;
-
-        getPreviewCube().scale.set(Math.abs(this.lastClickPos[0] - pos[0]) + 1.1, Math.abs(this.lastClickPos[1] - pos[1]) + 1.1, Math.abs(this.lastClickPos[2] - pos[2]) + 1.1);
-        getPreviewCube().position.set(
-            ((this.lastClickPos[0] + pos[0]) / 2) + 0.5,
-            ((this.lastClickPos[1] + pos[1]) / 2) + 0.5,
-            ((this.lastClickPos[2] + pos[2]) / 2) + 0.5,
-        );
-    }
-
-    async onPointerUp(event: PointerEvent)
-    {
-        try
-        {
-            let pos = this.getIntersectionPos(this.curX, this.curY, true);
-            if (!pos)
-                return;
-
-            // Make it so that floor-squares have at least one-cell of erasing.
-            if (this.lastClickPos[1] === -1 && pos[1] === -1)
-                pos[1] = 0;
-    
-            let actionData = [];
-            for (let x = Math.min(this.lastClickPos[0], pos[0]); x <= Math.max(this.lastClickPos[0], pos[0]); ++x)
-                for (let y = Math.min(this.lastClickPos[1], pos[1]); y <= Math.max(this.lastClickPos[1], pos[1]); ++y)
-                    for (let z = Math.min(this.lastClickPos[2], pos[2]); z <= Math.max(this.lastClickPos[2], pos[2]); ++z)
-                        if (store.state.builderData.currentSet.getAt(x, y, z))
-                            actionData.push({ pos: [x, y, z] });
-            await store.dispatch("builderData/place_briqs", actionData);
-        } finally {
-            this.fsm.switchTo("erase");
-        }
+    async doAction(pos: [number, number, number]) {
+        let actionData = [];
+        for (let x = Math.min(this.initialClickPos[0], pos[0]); x <= Math.max(this.initialClickPos[0], pos[0]); ++x)
+            for (let y = Math.min(this.initialClickPos[1], pos[1]); y <= Math.max(this.initialClickPos[1], pos[1]); ++y)
+                for (let z = Math.min(this.initialClickPos[2], pos[2]); z <= Math.max(this.initialClickPos[2], pos[2]); ++z)
+                    if (store.state.builderData.currentSet.getAt(x, y, z))
+                        actionData.push({ pos: [x, y, z] });
+        await store.dispatch("builderData/place_briqs", actionData);
     }
 }
