8a480ba1c64bb799532fdd3f1f9fad4b78e37f10
Lancelot de Ferrière <wraitii@gmail.com>
2022-04-01 10:30:55 +0200

    make the logo white all the time

--- src/components/builder/AlphaLogo.vue
+++ src/components/builder/AlphaLogo.vue
@@ -3,10 +3,11 @@ import { RealmsLogo } from '@/conf/realms';
 </script>
 
 <template>
-    <div class="absolute bottom-0 right-0 p-2">
+    <div class="absolute bottom-0 right-0 p-2 text-white">
         <a target="_blank" href="https://briqnft.notion.site/briqnft/briq-hub-ae6a1d92745044cc9c2274251a5212f3">
             <h5 v-if="CONF.theme === 'realms'" class="text-5xl font-bold drop-shadow-lg px-4 py-6 alpha-pill briq-logo flex flex-col items-center leading-[3rem]">
-                <span>briq</span><span class="text-3xl leading-[1.5rem]">x</span><span><RealmsLogo height="3.5rem" style="fill: rgba(var(--color-text));"/></span>
+                <!-- style="fill: rgba(var(--color-text));" -->
+                <span>briq</span><span class="text-3xl leading-[1.5rem]">x</span><span><RealmsLogo height="3.5rem" /></span>
             </h5>
             <h5 v-else="" class="text-5xl font-bold drop-shadow-lg px-4 py-6 alpha-pill briq-logo">briq</h5>
         </a>
