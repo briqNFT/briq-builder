63e4129b6f3a7454fae3d3ed2a1b51b1d150a0f8
Lancelot de Ferrière <wraitii@gmail.com>
2022-04-11 16:27:37 +0200

    fix selection flickering issue

--- src/components/builder/modals/BriqBrowser.vue
+++ src/components/builder/modals/BriqBrowser.vue
@@ -2,17 +2,20 @@
     <Window>
         <template #big-title><span><span class="briq-logo">briq</span> browser</span></template>
         <div class="browser grid grid-cols-2 gap-1">
-            <div @click="setMaterial('0x1')">Regular material</div>
-            <div @click="setMaterial('0x3')">Limited material</div>
-            <div @click="setMaterial('0x4')">Rare material</div>
-            <div @click="setMaterial('0x5')">Ultra-rare material</div>
+            <Btn @click="setMaterial('0x1')" :class="currentMat === '0x1' && 'active'">Regular material</Btn>
+            <Btn @click="setMaterial('0x3')" :class="currentMat === '0x3' && 'active'">Limited material</Btn>
+            <Btn @click="setMaterial('0x4')" :class="currentMat === '0x4' && 'active'">Rare material</Btn>
+            <Btn @click="setMaterial('0x5')" :class="currentMat === '0x5' && 'active'">Ultra-rare material</Btn>
         </div>
     </Window>
 </template>
 
 <style scoped>
-.browser > div {
-    @apply h-20 bg-accent p-2 text-center
+.browser > button {
+    @apply h-20
+}
+.active {
+    @apply bg-accent;
 }
 </style>
 
@@ -21,6 +24,11 @@ import { inputStore } from '@/builder/inputs/InputStore';
 
 import { defineComponent } from 'vue';
 export default defineComponent({
+    computed: {
+        currentMat() {
+            return inputStore.currentMaterial;
+        }
+    },
     methods: {
         setMaterial(mat: string) {
             inputStore.currentMaterial = mat;
