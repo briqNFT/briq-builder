64ac78eb63bf40c51ed702ac1c717f33c58fd9c9
Francis Berger <francisberger@MBP-de-Napoleon.lan>
2022-04-24 22:52:58 +0200

    feat(modals): added modal to confirm clear all

--- src/components/builder/inputs/Erase.vue
+++ src/components/builder/inputs/Erase.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="w-full">
-        <Btn class="w-full" @click="clear">Clear All</Btn>
+    <div v-if="getUsedBriqsNb" class="w-full">
+        <Btn class="w-full" @click="openModalToConfirmClear">Clear All</Btn>
     </div>
 </template>
 
 <script lang="ts">
+import ConfirmClearAll from '../modals/ConfirmClearAll.vue';
+import { pushModal } from '../../Modals.vue';
 import { defineComponent } from 'vue';
 export default defineComponent({
-    data() {
-        return {};
-    },
-    methods: {
-        async clear() {
-            await this.$store.dispatch('builderData/clear');
+    computed: {
+        getUsedBriqsNb(): number {
+            let used = 0;
+            for (let mat in this.$store.state.builderData.currentSet.usedByMaterial)
+                used += this.$store.state.builderData.currentSet.usedByMaterial[mat];
+            return used;
         },
     },
-});
+    methods: {
+        async openModalToConfirmClear(): Promise<void> {
+            const userConfirmed = await pushModal(ConfirmClearAll, { asModal: true });
+            if (userConfirmed) await this.$store.dispatch("builderData/clear");;
+        },
+    }
+})
 </script>
---
+++ src/components/builder/modals/ConfirmClearAll.vue
@@ -0,0 +1,12 @@
+<template>
+    <Window size="w-max">
+        <template #big-title>Clear all</template>
+         <div class="my-4">
+            <p>All work will be cleared.</p>
+        </div>
+        <div class="flex justify-between gap-4">
+            <Btn @click="$emit('close', false)">Cancel</Btn>
+            <Btn @click="$emit('close', true)">Confirm Clear</Btn>
+        </div>
+    </Window>
+</template>
