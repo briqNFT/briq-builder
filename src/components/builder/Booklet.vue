9391b0a7735716664ab0839e149f7acf090bc4e9
Lancelot de Ferrière <wraitii@gmail.com>
2022-06-07 16:45:40 +0200

    Pseudo progress bar for booklet

--- src/components/builder/Booklet.vue
+++ src/components/builder/Booklet.vue
@@ -5,7 +5,7 @@ import type { SetData } from '@/builder/SetData';
 import { CONF } from '@/Conf';
 import { markRaw, onBeforeMount, ref, watchEffect } from 'vue';
 import { useStore } from 'vuex';
-
+import Tooltip from '@/components/generic/Tooltip.vue';
 import Slider from '../generic/Slider.vue';
 import { pushModal } from '../Modals.vue';
 import { useBooklet } from './BookletComposable';
@@ -13,91 +13,36 @@ import ExportSetBookletVue from './modals/ExportSetBooklet.vue';
 
 const currentPage = ref(1)
 
-const booklet = ref("");
-
-const bookletData = ref(null as any);
-
-onBeforeMount(() => {
-    let data = window.localStorage.getItem('briq_current_booklet');
-    if (!data)
-        return;
-    try {
-        booklet.value = data;
-        fetch(booklet.value + '/shape.json').then(
-            async data => {
-                let metadata = await data.json();
-                bookletData.value = markRaw(metadata);
-                const palette = palettesMgr.getCurrent();
-                palette.reset(false);
-                for (const key in CONF.defaultPalette)
-                    delete CONF.defaultPalette[key];
-                const colors = new Set();
-                for (const briq of metadata.briqs) {
-                    CONF.defaultPalette[packPaletteChoice(briq.data.material, briq.data.color)] = briq.data.color;
-                    console.log(CONF.defaultPalette);
-                    colors.add(packPaletteChoice(briq.data.material, briq.data.color));
-                }
-                colors.forEach(col => palette.addChoice({ key: col }, col));
-                const choice = palette.getFirstChoice();
-                inputStore.currentColor = choice.color;
-                inputStore.currentMaterial = choice.material;
-            }
-        ).catch(() => {
-            bookletData.value = undefined;
-            window.localStorage.removeItem('briq_current_booklet');
-        })
-    } catch(e) {
-        window.localStorage.removeItem('briq_current_booklet');
-    }
-})
-
-const shapeIsValid = ref(false);
-
 const store = useStore();
-watchEffect(() => {
-    if (!bookletData.value)
-        return;
-    const set = store.state.builderData.currentSet as SetData;
-    set.briqs_;
-    const currentBriqs = set.getAllBriqs();
-    const targetBriqs = bookletData.value.briqs.slice();
-    if (currentBriqs.length !== targetBriqs.length) {
-        shapeIsValid.value = false;
-        return;
-    }
-    let match = 0;
-    for (const a of targetBriqs)
-    {
-        for (const b of currentBriqs)
-        {
-            if (a.pos[0] === b.position[0] && a.pos[1] === b.position[1] && a.pos[2] === b.position[2]
-                 && a.data.color === b.color && a.data.material === b.material)
-            {
-                match++;
-                break;
-            }
-        }
-    }
-    shapeIsValid.value = match === targetBriqs.length
-})
 
 const onMint = async () => {
     await pushModal(ExportSetBookletVue, { set: store.state.builderData.currentSet });
 }
 
-const { getImgSrc } = useBooklet();
+const {
+getImgSrc,
+    shapeValidity,
+    booklet,
+    bookletData,
+} = useBooklet();
 </script>
 
 <template>
     <div v-if="booklet" class="absolute top-0 bottom-0 right-0 pointer-events-none flex flex-col alternate-buttons m-4">
         <div class="grow basis-[6rem]"></div>
-        <div class="pointer-events-auto bg-base rounded-md p-4 flex-col flex items-center gap-2">
+        <div class="pointer-events-auto bg-base rounded-md p-4 pb-6 flex-col flex items-center gap-2 relative">
             <template v-if="!!bookletData">
+                <Tooltip :tooltip="`Construction progress: ${Math.floor(shapeValidity*100)}%`">
+                    <div class="progress-bar absolute left-0 bottom-0 rounded-b-md bg-red-600 h-4 w-full border-t-4 border-white">
+                        <div v-if="shapeValidity < 1" class="progress-bar absolute left-0 top-0 rounded-b-md rounded-r-md bg-green-600 h-3" :style="{ width: `${shapeValidity*100}%`}"></div>
+                        <div v-else=""                class="progress-bar absolute left-0 top-0 rounded-b-md bg-green-600 h-3" :style="{ width: `${shapeValidity*100}%`}"></div>
+                    </div>
+                </Tooltip>
                 <h2>{{ bookletData.name }}</h2>
                 <p>Eye Hint</p>
                 <img :src="getImgSrc(booklet, currentPage)">
                 <p class="w-full"><Slider :min="1" :max="+bookletData.nb_pages || 1" v-model="currentPage"/></p>
-                <Btn class="w-[10rem] my-1" :disabled="!shapeIsValid" @click="onMint">Mint</Btn>
+                <Btn class="w-[10rem] my-1" :disabled="shapeValidity < 1" @click="onMint">Mint</Btn>
             </template>
             <template v-else="">
                 <p>Loading booklet data</p>
--- src/components/builder/BookletComposable.ts
+++ src/components/builder/BookletComposable.ts
@@ -1,8 +1,89 @@
+import { inputStore } from '@/builder/inputs/InputStore';
+import { packPaletteChoice, palettesMgr } from '@/builder/Palette';
+import type { SetData } from '@/builder/SetData';
+import { CONF } from '@/Conf';
+import { markRaw, onBeforeMount, ref, watchEffect } from 'vue';
+import { useStore } from 'vuex';
 
 export function useBooklet() {
     const getImgSrc = (booklet: string, page: number) => `/${booklet}/step_${page - 1}.png`;
 
+    const booklet = ref("");
+
+    const bookletData = ref(null as any);
+
+    onBeforeMount(() => {
+        let data = window.localStorage.getItem('briq_current_booklet');
+        if (!data)
+            return;
+        try {
+            booklet.value = data;
+            fetch(booklet.value + '/shape.json').then(
+                async data => {
+                    let metadata = await data.json();
+                    bookletData.value = markRaw(metadata);
+                    const palette = palettesMgr.getCurrent();
+                    palette.reset(false);
+                    for (const key in CONF.defaultPalette)
+                        delete CONF.defaultPalette[key];
+                    const colors = new Set();
+                    for (const briq of metadata.briqs) {
+                        CONF.defaultPalette[packPaletteChoice(briq.data.material, briq.data.color)] = briq.data.color;
+                        colors.add(packPaletteChoice(briq.data.material, briq.data.color));
+                    }
+                    colors.forEach(col => palette.addChoice({ key: col }, col));
+                    const choice = palette.getFirstChoice();
+                    inputStore.currentColor = choice.color;
+                    inputStore.currentMaterial = choice.material;
+                }
+            ).catch(() => {
+                bookletData.value = undefined;
+                window.localStorage.removeItem('briq_current_booklet');
+            })
+        } catch(e) {
+            window.localStorage.removeItem('briq_current_booklet');
+        }
+    })
+
+    const store = useStore();
+
+    const shapeValidity = ref(0);
+
+    watchEffect(() => {
+        if (!bookletData.value)
+            return;
+        const set = store.state.builderData.currentSet as SetData;
+        set.briqs_;
+        const currentBriqs = set.getAllBriqs();
+        const targetBriqs = bookletData.value.briqs.slice();
+        let match = 0;
+        for (const a of targetBriqs)
+        {
+            let found = false;
+            for (let i = 0; i < currentBriqs.length; ++i)
+            {
+                const b = currentBriqs[i];
+                if (a.pos[0] === b.position[0] && a.pos[1] === b.position[1] && a.pos[2] === b.position[2])
+                {
+                    if (a.data.color === b.color && a.data.material === b.material)
+                        match++;
+                    else
+                        match += 0.5;
+                    found = true;
+                    currentBriqs.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        match -= currentBriqs.length * 0.9;
+        shapeValidity.value = Math.min(1, Math.max(0, match / targetBriqs.length));
+    })
+
+
     return {
-        getImgSrc
+        getImgSrc,
+        shapeValidity,
+        booklet,
+        bookletData,
     };
 };
