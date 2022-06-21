3523a424d0b998906755d4a8fb63eb33a61b863c
Lancelot de Ferrière <wraitii@gmail.com>
2022-06-21 15:13:48 +0200

    Rework landing page

--- src/components/builder/genesis/GenesisMint.vue
+++ src/components/builder/genesis/GenesisMint.vue
@@ -7,6 +7,9 @@ import Interoperable from '@/assets/landing/interoperable.svg';
 import OnChain from '@/assets/landing/onchain.svg';
 
 import BriqsOverlay from '@/assets/landing/briqs.svg';
+import BriqsOverlayB from '@/assets/landing/briqs-bottom.svg';
+import BriqsOverlayT from '@/assets/landing/briqs-top.svg';
+import BriqsOverlayR from '@/assets/landing/briqs-right.svg';
 
 import FabricIcon from '@/assets/landing/fabric.png';
 import FirstIcon from '@/assets/landing/frst.svg';
@@ -22,19 +25,18 @@ const modelViewerLoading = ref(true);
 const modelViewerLoadingPromise = import('@google/model-viewer');
 modelViewerLoadingPromise.then(() => modelViewerLoading.value = false);
 
-const briqOverlay = ref(null);
+const briqParallax = ref(0);
 
 const onScroll = (_event: Event) => {
-    if(briqOverlay.value)
-        briqOverlay.value.style.bottom = `${-50 + window.scrollY / 6}px`;
+    briqParallax.value = window.scrollY / 6;
 }
 
 onBeforeMount(() => {
-    document.body.style.backgroundColor = 'rgb(var(--color-base))';
+    //    document.body.style.backgroundColor = 'rgb(var(--color-base))';
     window.addEventListener('scroll', onScroll);
 });
 onBeforeUnmount(() => {
-    document.body.style.backgroundColor = '';
+    //    document.body.style.backgroundColor = '';
     window.removeEventListener('scroll', onScroll);
 });
 
@@ -62,34 +64,39 @@ onMounted(async () => {
 <template>
     <!-- Make it absolute so h-screen works below -->
     <Header class="absolute left-0 right-0 z-50"/>
-    <div class="container m-auto relative h-screen flex flex-col overflow-hidden">
-        <div class="grow-[3]"/>
-        <div class="z-50 flex flex-wrap justify-around items-center">
-            <div class="relative p-20">
-                <!-- Hack to add stroke, should work on anything relevant -->
-                <div class="absolute z-[-1] md:visible invisible">
-                    <h1 style="-webkit-text-stroke: 8px rgb(var(--color-base));" class="text-max font-bold leading-tighter my-8">Seize the <span class="briq-logo !m-0 !font-extrabold">briqs</span><br>of construction</h1>
-                    <p style="-webkit-text-stroke: 3px rgb(var(--color-base));" class="text-lg font-normal">briq is a powerful Web3 toy which aims at<br>developping imagination and creativity</p>
+    <div class="bg-base">
+        <div class="container m-auto relative h-screen p-[6rem] flex flex-col overflow-hidden">
+            <div class="grow-[3]"/>
+            <div class="z-50 flex flex-wrap justify-around items-center">
+                <div class="relative p-20">
+                    <div>
+                        <h1 class="text-max font-bold leading-tighter my-8">Seize the <span class="briq-logo !m-0 !font-extrabold">briqs</span><br>of construction</h1>
+                        <p class="text-lg font-normal">briq is a powerful Web3 toy which aims at<br>developping imagination and creativity</p>
+                    </div>
                 </div>
-                <div>
-                    <h1 class="text-max font-bold leading-tighter my-8">Seize the <span class="briq-logo !m-0 !font-extrabold">briqs</span><br>of construction</h1>
-                    <p class="text-lg font-normal">briq is a powerful Web3 toy which aims at<br>developping imagination and creativity</p>
+                <component
+                    v-if="!modelViewerLoading"
+                    :is="modelViewer" class="flex-1 min-w-[10rem] h-full w-full"
+                    reveal="manual" loading="eager" shadow-intensity="0.5" shadow-softness="1" disable-zoom camera-controls auto-rotate="true"
+                    field-of-view="40deg" camera-target="2m 23m 2m"
+                    style="background-color: unset; --poster-color: transparent"/>
+                <!-- Manually inlined rocket cover, around 20KB -->
+                <div v-else class="flex-1 min-w-[10rem] flex justify-center"><img :src="InlinedRocketCover"></div>
+            </div>
+            <div class="grow-[6]"/>
+        </div>
+        <div class="container m-auto absolute top-0 left-0 right-0">
+            <div class="relative w-full h-screen">
+                <div class="pointer-events-none user-select-none absolute z-50 left-0" :style="{ top: `${150 - briqParallax}px` }">
+                    <BriqsOverlayT/>
+                </div>
+                <div class="pointer-events-none user-select-none absolute z-50 left-0" :style="{ bottom: `${-120 + briqParallax}px` }">
+                    <BriqsOverlayB class="w-[1432px] h-[780px] relative top-[150px] tall-md:top-[0px]"/>
+                </div>
+                <div class="pointer-events-none user-select-none absolute z-50 lg:right-[-600px] xl:right-[-250px] 2xl:right-[-100px]" :style="{ top: `${150 - briqParallax}px` }">
+                    <BriqsOverlayR class="w-[1432px] h-[780px]"/>
                 </div>
             </div>
-            <component
-                v-if="!modelViewerLoading"
-                :is="modelViewer" class="flex-1 min-w-[10rem] h-full w-full"
-                reveal="manual" loading="eager" shadow-intensity="0.5" shadow-softness="1" disable-zoom camera-controls auto-rotate="true"
-                field-of-view="40deg" camera-target="2m 23m 2m"
-                style="background-color: unset; --poster-color: transparent"/>
-            <!-- Manually inlined rocket cover, around 20KB -->
-            <div v-else class="flex-1 min-w-[10rem] flex justify-center"><img :src="InlinedRocketCover"></div>
-        </div>
-        <div class="grow-[4]"/>
-    </div>
-    <div class="container m-auto relative">
-        <div class="pointer-events-none user-select-none absolute z-50 left-[-250px] md:left-0" style="bottom: -50px;" ref="briqOverlay">
-            <BriqsOverlay class="w-[1432px] h-[780px]"/>
         </div>
     </div>
     <div
@@ -101,40 +108,54 @@ onMounted(async () => {
             <span class="w-max animate-text-loop whitespace-pre">briq is NFT matter • </span>
         </p>
     </div>
-    <div class="bg-[#FEF6F3]">
+    <div>
         <div class="container m-auto p-20">
             <div class="grid grid-cols-3 gap-8 explanations">
                 <div>
-                    <p class="flex justify-center items-center h-[12rem] pb-4"><Composable/></p>
-                    <h2>Composable</h2>
-                    <p>Association of briqs are NFTs called sets.</p>
-                    <p>Sets can be disassembled to get the briqs back and create something new.</p>
-                    <p>They can be combined to create even more complex structures.</p>
+                    <div class="flex justify-center items-center h-[10rem] pb-4">
+                        <div class="grid grid-cols-3 block-grid gap-2">
+                            <p class="bg-accent"/><p class="bg-accent"/><p class="bg-base"/>
+                            <p class="bg-accent"/><p class="bg-base"/><p class="bg-accent"/>
+                        </div>
+                    </div>
+                    <h2>Compose</h2>
+                    <p>briqs are construction blocks stored on the blockchain.</p>
+                    <p>Technically, they’re ERC-1155 tokens stored on Starknet, an Ethereum layer two solution allowing cheap transaction costs.</p>
+                    <p>Your briqs are yours, forever.</p>
                 </div>
                 <div>
-                    <p class="flex justify-center items-center h-[12rem] pb-4"><Interoperable/></p>
-                    <h2>Interoperable</h2>
-                    <p>briqs and sets can be used in any game and can be interpreted as you see fit.</p>
-                    <p>Build a car and race. Tear it apart and build a plane.</p>
-                    <p>They can be combined to create even more complex structures.</p>
+                    <div class="flex justify-center items-center h-[10rem] pb-4">
+                        <div class="grid grid-cols-2 block-grid gap-2">
+                            <p class="bg-accent"/><p class="bg-accent"/>
+                            <p class="bg-accent"/><p class="bg-accent"/>
+                        </div>
+                    </div>
+                    <h2>Build</h2>
+                    <p>briq can be assembled to create NFTs. These NFT are called sets.</p>
+                    <p>Transfer, sell, lend, break your NFTs any way you want.</p>
+                    <p>Disassemble your set to get your briqs back and build something new.</p>
                 </div>
                 <div>
-                    <p class="flex justify-center items-center h-[12rem] pb-4"><OnChain/></p>
-                    <h2>On-chain</h2>
-                    <p>Association of briqs are NFTs called sets.</p>
-                    <p>Sets can be disassembled to get the briqs back and create something new.</p>
-                    <p>They can be combined to create even more complex structures.</p>
+                    <div class="flex justify-center items-center h-[10rem] pb-4">
+                        <div class="grid grid-cols-3 block-grid gap-2">
+                            <p class="bg-base"/><p class="bg-accent"/><p class="bg-base"/>
+                            <p class="bg-accent"/><p class="bg-accent"/><p class="bg-accent"/>
+                        </div>
+                    </div>
+                    <h2>Play</h2>
+                    <p>briqs sets are highly interoperable NFTs. Carry them around with you to the nearest metaverse. </p>
+                    <p>Integrate briq in any way you want. Seize the briqs of construction.</p>
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg-black text-white">
-        <div class="container m-auto flex flex-col gap-20 py-20 items-center justify-between">
-            <h1>Starknet City</h1>
-            <routerLink :to="{ name: 'Theme', params: { theme: 'test' } }">
+    <div class="container mx-auto mb-20 px-20">
+        <routerLink :to="{ name: 'Theme', params: { theme: 'test' } }">
+            <div class="bg-black text-white rounded-lg flex flex-col gap-20 py-20 items-center justify-between">
+                <h1>Starknet City</h1>
                 <p>Discover the briq genesis sale <i class="fa-solid fa-arrow-right"/></p>
-            </routerlink>
-        </div>
+            </div>
+        </routerlink>
     </div>
     <div class="bg-[#FEF6F3]">
         <div class="container m-auto p-20">
@@ -161,6 +182,9 @@ h2 {
 }
 .explanations p {
     @apply my-4;
+}
+.block-grid p {
+    @apply h-8 w-8 rounded-sm p-0 m-0;
 }
 </style>
 
