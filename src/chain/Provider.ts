428c760299ece27edd247bb9851207c9189eef8c
Lancelot de Ferrière <wraitii@gmail.com>
2022-06-24 16:39:30 +0200

    fixup for mock chain in test

--- src/chain/Provider.ts
+++ src/chain/Provider.ts
@@ -1,4 +1,5 @@
 import { Provider } from '@/starknet_wrapper';
+import getBaseUrl from '@/url';
 
 import { CHAIN_NETWORKS, getCurrentNetwork } from './Network';
 
@@ -8,7 +9,7 @@ export function getProvider(): Provider {
 
 export function getProviderForNetwork(network: CHAIN_NETWORKS): Provider {
     return {
-        'mock': new Provider({ baseUrl: 'http://localhost:5050/mock_chain' }),
+        'mock': new Provider({ baseUrl: getBaseUrl() + '/mock_chain' }),
         'localhost': new Provider({ baseUrl: 'http://localhost:5000' }),
         'starknet-testnet': new Provider({ network: 'goerli-alpha' }),
         'starknet-testnet-legacy': new Provider({ network: 'goerli-alpha' }),
