c69e70f6efdc7f968d6d19aa99ac41c15c5874ac
Lancelot de Ferrière <wraitii@gmail.com>
2022-08-09 14:43:09 +0200

    use nesting for buttons

--- src/components/generic/Button.vue
+++ src/components/generic/Button.vue
@@ -37,93 +37,93 @@ defineProps<{
     @apply text-sm md:text-md font-normal leading-none;
     @apply select-none;
     @apply relative;
-}
 
-#app button.icon > i, #app button.icon > svg {
-    @apply w-6 pr-1 inline-flex justify-center items-baseline;
-}
+    &.icon > i, &.icon > svg {
+        @apply w-6 pr-1 inline-flex justify-center items-baseline;
+    }
 
-/* No background design */
-#app button:not(.btn):not(.nostyle) {
-    @apply text-text-on-background;
-}
+    /* no-background variant */
+    &:not(.btn):not(.nostyle) {
+        @apply text-text-on-background;
 
-#app button:not(.btn):not(.nostyle):not(:disabled):active, #app button:not(.btn):not(.nostyle):not(:disabled).forceActive {
-    @apply text-primary;
-}
+        &::before {
+            content: '';
+            z-index: -1;
+            @apply w-full h-full absolute top-0 left-0;
+            @apply rounded-sm;
+        }
 
-#app button:not(.btn):not(.nostyle)::before {
-    content: '';
-    z-index: -1;
-    @apply w-full h-full absolute top-0 left-0;
-    @apply rounded-sm;
-}
+        &:not(:disabled) {
+            &:active, &.forceActive {
+                @apply text-primary;
+            }
 
-#app button:not(.btn):not(.nostyle):not(:disabled):hover::before {
-    @apply bg-grad-lighter;
-}
+            &:hover::before {
+                @apply bg-grad-lighter;
+            }
+            &:active::before, &.forceActive::before {
+                @apply bg-grad-lighter;
+            }
+        }
 
-#app button:not(.btn):not(.nostyle):not(:disabled):active::before {
-    @apply bg-grad-lighter;
-}
+        &:disabled {
+            @apply text-grad-darker;
+        }
+    }
 
-#app button:not(.btn):not(.nostyle):disabled {
-    @apply text-grad-darker;
-}
+    &.btn {
+        @apply text-text-on-primary;
+        @apply h-[2rem] md:h-[2.5rem];
+        @apply px-1 md:px-3 py-2 md:py-4;
 
+        /* Primary design */
+        /* Use a pseudo-element to avoid the layout changing when the size changes */
+        &::before {
+            content: '';
+            z-index: -1;
+            @apply w-full h-full absolute top-0 left-0;
+            @apply bg-primary border border-grad-darker rounded;
+        }
 
-/* Primary design */
-#app .btn {
-    @apply text-text-on-primary;
-    @apply h-[2rem] md:h-[2.5rem];
-    @apply px-1 md:px-3 py-2 md:py-4;
-}
+        &:not(:disabled) {
+            &:hover::before {
+                @apply bg-primary-lighter;
+            }
+            &:active::before {
+                /* 2px on either side */
+                @apply w-[calc(100%-0.125rem)] h-[calc(100%-0.125rem)] top-[0.0625rem] left-[0.0625rem];
+                @apply bg-primary-lighter;
+            }
+        }
 
-/* Use a pseudo-element to avoid the layout changing when the size changes */
-#app .btn::before {
-    content: '';
-    z-index: -1;
-    @apply w-full h-full absolute top-0 left-0;
-    @apply bg-primary border border-grad-darker rounded;
-}
+        &:disabled {
+            --tw-text-opacity: 0.5;
+        }
 
-#app .btn:not(:disabled):hover::before {
-    @apply bg-primary-lighter;
-}
+        &:disabled::before {
+            @apply bg-primary-lightest;
+        }
 
-#app .btn:not(:disabled):active::before {
-    /* 2px on either side */
-    @apply w-[calc(100%-0.125rem)] h-[calc(100%-0.125rem)] top-[0.0625rem] left-[0.0625rem];
-    @apply bg-primary-lighter;
-}
+        /* Secondary button design */
+        &.secondary {
+            @apply text-text-on-background;
+            &::before {
+                @apply bg-grad-lightest border-grad-light;
+            }
 
-#app .btn:disabled::before {
-    @apply bg-primary-lightest;
-}
-
-/* Secondary button design */
-#app .btn.secondary {
-    @apply text-text-on-background;
-}
-
-#app .btn.secondary::before {
-    @apply bg-grad-lightest border-grad-light;
-}
-
-#app .btn.secondary:not(:disabled):hover::before {
-    @apply bg-grad-lighter;
-}
-
-#app .btn.secondary:not(:disabled):active::before {
-    @apply bg-grad-lighter;
-}
-
-#app .btn.secondary:disabled::before {
-    @apply bg-grad-lightest;
-}
-
-#app .btn.secondary:disabled {
-    --tw-text-opacity: 0.5;
+            &:not(:disabled) {
+                &:hover::before {
+                    @apply bg-grad-lighter;
+                }
+                &:active::before {
+                    @apply bg-grad-lighter;
+                }
+            }
+            &:disabled::before {
+                @apply bg-grad-lightest;
+            }
+        }
+    }
 }
 
 </style>
