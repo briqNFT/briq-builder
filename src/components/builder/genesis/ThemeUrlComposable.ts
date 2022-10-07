86fdb33cf814e286fda1f4ad6a6b37e6fb1fd85b
Lancelot de Ferrière <wraitii@gmail.com>
2022-10-07 15:49:05 +0200

    Fix theme listings

--- src/components/builder/genesis/ThemeUrlComposable.ts
+++ src/components/builder/genesis/ThemeUrlComposable.ts
@@ -4,8 +4,10 @@ import { getCurrentNetwork } from '@/chain/Network';
 export function useThemeURLs() {
     const themeCoverUrl = (theme: string, network: string = getCurrentNetwork()) => backendManager.getRoute(`${network}/${theme}/cover.jpg`);
     const themeLogoSrcSet = (theme: string, network: string = getCurrentNetwork()) => backendManager.getRoute(`${network}/${theme}/logo.png`) + ' 2x';
+    const themeSplashSrcSet = (theme: string, network: string = getCurrentNetwork()) => backendManager.getRoute(`${network}/${theme}/splash.jpg`) + ' 2x';
     return {
         themeCoverUrl,
         themeLogoSrcSet,
+        themeSplashSrcSet,
     }
 }
--- src/components/builder/genesis/ThemesListing.vue
+++ src/components/builder/genesis/ThemesListing.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Header from '@/components/landing_page/Header.vue';
 import Footer from '@/components/landing_page/Footer.vue';
+import briqIcon from '@/assets/landing/briq-icon.svg';
+
 import { useGenesisStore } from '@/builder/GenesisStore';
 import { computed, ref } from 'vue';
 import { backendManager } from '@/Backend';
 import { CHAIN_NETWORKS, getCurrentNetwork } from '@/chain/Network';
+import { useThemeURLs } from './ThemeUrlComposable';
 
 const genesisStore = useGenesisStore();
 
@@ -23,23 +26,38 @@ const themeList = computed(() => {
     return (_themeList.value[1] || []) as string[];
 });
 
+const { themeSplashSrcSet } = useThemeURLs();
+
 </script>
 
 
 <style scoped>
+.popOver > div {
+    @apply relative top-0 transition-all duration-300;
+}
+.popOver:hover > div {
+    @apply top-[-0.5rem] shadow-lg;
+}
 </style>
 
 <template>
     <Header/>
-    <div class="container m-auto">
-        <h1>Themes</h1>
-        <div class="grid grid-cols-3 gap-4 my-8">
-            <RouterLink v-for="theme of themeList" :key="theme" :to="{ name: 'Theme', params: { theme: theme } }">
-                <div class="h-[15rem] flex flex-col justify-center items-center gap-8 text-white bg-black rounded-lg">
-                    <h2>{{ genesisStore.themedata[theme]._data?.name }}</h2>
-                    <p>{{ genesisStore.themedata[theme]._data?.description }}</p>
+    <div class="container m-auto min-h-[40rem]">
+        <h3 class="mt-10 mb-4">Themes</h3>
+        <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
+            <RouterLink class="popOver" v-for="theme of themeList" :key="theme" :to="{ name: 'Theme', params: { theme: theme } }">
+                <div class="h-[16rem] flex flex-col justify-center items-center gap-2 rounded-lg relative">
+                    <div class="absolute pointer-events-none overflow-hidden rounded-lg h-full w-full">
+                        <img class="max-w-none max-h-none w-auto h-full relative top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]" :srcset="themeSplashSrcSet(theme)" :alt="`Theme splash for ${theme}`">
+                    </div>
+                    <h4 class="text-md text-white absolute bottom-3 left-4">{{ genesisStore.themedata[theme]._data?.name }}</h4>
                 </div>
             </RouterLink>
+            <div class="h-[16rem] flex flex-col justify-center items-center gap-2 bg-grad-lightest border border-grad-light rounded-lg">
+                <p class="text-primary">New themes</p>
+                <h3>COMING SOON</h3>
+                <briqIcon class="scale-[1.5]"/>
+            </div>
         </div>
     </div>
     <Footer/>
