9958ba8c757e2724c3103cdcf9046fa179ef9768
Lancelot de Ferrière <wraitii@gmail.com>
2022-10-11 15:14:11 +0200

    More attempt at fixing shadows

--- src/components/builder/genesis/UnboxingGraphicsLight.ts
+++ src/components/builder/genesis/UnboxingGraphicsLight.ts
@@ -261,6 +261,7 @@ export async function setupScene(quality: SceneQuality = SceneQuality.ULTRA) {
     light.position.set(1, 2, -0.5);
     light.shadow.mapSize = new THREE.Vector2(1024, 1024);
     light.shadow.radius = 20;
+    light.shadow.blurSamples = 20;
     light.shadow.camera.far = 5.0;
     light.shadow.bias = -0.004;
     //light.shadow.normalBias = 0.08;
