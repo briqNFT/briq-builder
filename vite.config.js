ae993489f1e416040ec972f2d187c03ec6c3af54
Lancelot de Ferrière <wraitii@gmail.com>
2022-10-14 14:11:22 +0200

    Minor tweaks to index.html for faster loading

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import analyze from 'rollup-plugin-analyzer';
@@ -20,7 +21,21 @@ const injectNpmVersion = () => {
     };
 };
 
-import path from 'path';
+const htmlPlugin = () => {
+    return {
+        name: 'html-transform',
+        transformIndexHtml(html, ctx) {
+            if (!ctx?.chunk?.dynamicImports)
+                return html;
+            // Prefetch some scripts that we'll load and can take some time to load. Can help, can not help, depending.
+            const preloadScripts = ctx.chunk.dynamicImports.filter(x => x.indexOf('Dispatch') !== -1 || x.indexOf('three') !== -1)
+            return html.replace(
+                '</script>',
+                '</script>\n    ' + preloadScripts.map(x => `<link rel="prefetch" as="script" crossorigin href="${x}">`).join('\n    '),
+            )
+        },
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -39,5 +54,5 @@ export default defineConfig(({ mode }) => ({
         environment: 'jsdom',
     },
     // Uncomment to see details on what's going with rollup
-    plugins: [vue(), svgLoader(), injectNpmVersion()],//, analyze({ showExports: true }), visualizer({ template: 'treemap', sourcemap: true })],
+    plugins: [vue(), svgLoader(), injectNpmVersion(), htmlPlugin()],//, analyze({ showExports: true }), visualizer({ template: 'treemap', sourcemap: true })],
 }));
