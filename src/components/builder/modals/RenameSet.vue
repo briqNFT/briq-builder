fa6057318b09222a982225b13ca8e3f86f140897
Lancelot de Ferrière <wraitii@gmail.com>
2022-10-20 17:12:43 +0200

    Clean up renameSet modal

--- src/components/builder/modals/RenameSet.vue
+++ src/components/builder/modals/RenameSet.vue
@@ -12,9 +12,11 @@ const props = defineProps<{
 const emit = defineEmits(['close'])
 const setData = setsManager.getInfo(props.set).getSet();
 const name = ref(setData.getName());
+const description = ref(setData.description);
 
 const save = () => {
     currentSet.value.name = name.value;
+    currentSet.value.description = description.value;
     emit('close');
 }
 </script>
@@ -22,14 +24,24 @@ const save = () => {
 <template>
     <Window class="!w-auto xl:max-w-[49%] lg:max-w-[80%] max-w-full">
         <template #title>Rename set</template>
-        <p>Set {{ set }}</p>
-        <p class="break-words">Current name: {{ setData.getName() }}</p>
-        <p class="md:block hidden">
-            New name: <input v-model="name" type="text" maxlength="200" minlength="1" size="60">
-        </p>
-        <p class="md:hidden block">
-            New name: <input v-model="name" type="text" maxlength="200" minlength="1" size="30">
-        </p>
-        <button class="btn float-right my-4" @click="save">Save</button>
+        <div class="my-4">
+            <p class="mb-2">Name</p>
+            <p class="md:block hidden">
+                <input v-model="name" type="text" maxlength="200" minlength="1" size="60">
+            </p>
+            <p class="md:hidden block">
+                <input v-model="name" type="text" maxlength="200" minlength="1" size="30">
+            </p>
+        </div>
+        <div class="my-4">
+            <p class="mb-2">Description</p>
+            <p class="md:block hidden">
+                <textarea v-model="description" cols="60"/>
+            </p>
+            <p class="md:hidden block">
+                <textarea v-model="description" cols="30"/>
+            </p>
+        </div>
+        <button class="btn float-right my-6" @click="save">Save</button>
     </Window>
 </template>
--- src/components/generic/Window.vue
+++ src/components/generic/Window.vue
@@ -15,8 +15,8 @@ const close = () => window.value.dispatchEvent(new CustomEvent('_close', { bubbl
 </script>
 
 <template>
-    <div ref="window" :class="'container p-0 rounded-lg bg-grad-lightest alternate-buttons m-2 sm:m-4 md:m-8 shadow-xl relative ' + _size">
-        <div class="relative h-full p-2 sm:p-4 md:p-6">
+    <div ref="window" :class="'container p-0 rounded-md md:rounded-lg bg-grad-lightest alternate-buttons m-2 sm:m-4 md:m-8 shadow-xl relative ' + _size">
+        <div class="relative h-full p-4 md:p-6">
             <div class="flex justify-between items-center pb-6">
                 <h3 class="font-semibold w-full" v-if="!!$slots.title"><slot name="title"/></h3>
                 <h2 class="font-semibold w-full" v-if="!!$slots['big-title']"><slot name="big-title"/></h2>
