687d2605f139ca3d9d1d5db871decfc158ba999e
Lancelot de Ferrière <wraitii@gmail.com>
2022-11-18 18:01:33 +0100

    Fix notification links

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -38,39 +38,39 @@ p {
         </template>
         <template v-else-if="notif.type === 'box_purchase_started'">
             <p>The transaction to purchase your box was sent.</p>
-            <p>Box: <RouterLink class="text-primary" :to="`box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <template v-else-if="notif.type === 'box_purchase_pending'">
             <p>Your box purchase transaction is pending.<br>You can now start unboxing.</p>
-            <p>Box: <RouterLink class="text-primary" :to="`box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <template v-else-if="notif.type === 'box_purchase_confirmed'">
             <p>Your box purchase was confirmed on StarkNet.</p>
-            <p>Box: <RouterLink class="text-primary" :to="`box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <template v-else-if="notif.type === 'box_purchase_failure'">
             <p>There was an error with the box purchase transaction.</p>
             <p v-if="toast"><a :href="ExplorerTxUrl(notif.data.tx_hash)" target="_blank" class="text-primary">See details on StarkScan</a></p>
-            <p>Box: <RouterLink class="text-primary" :to="`product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
 
         <template v-else-if="notif.type === 'box_unbox_started'">
             <p>The unboxing transaction was sent.</p>
-            <p>Box: <RouterLink class="text-primary" :to="`box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <template v-else-if="notif.type === 'box_unbox_confirmed'">
             <p>Your unboxing was confirmed on StarkNet.</p>
-            <p>Box: <RouterLink class="text-primary" :to="`box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/box/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <template v-else-if="notif.type === 'box_unbox_failure'">
             <p>There was an error with the unbox transaction.</p>
             <p v-if="toast"><a :href="ExplorerTxUrl(notif.data.tx_hash)" target="_blank" class="text-primary">See details on StarkScan</a></p>
-            <p>Box: <RouterLink class="text-primary" :to="`product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Box: <RouterLink class="text-primary" :to="`/product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
 
         <template v-else-if="['tentative_bid', 'confirmed_bid', 'pending_bid', 'rejected_bid'].indexOf(notif.type) !== -1">
             <p>Bid: <span class="font-medium">{{ readableNumber(notif.data.amount) }} {{ readableUnit(notif.data.amount) }}</span></p>
-            <p>Item: <RouterLink class="text-primary" :to="`product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
+            <p>Item: <RouterLink class="text-primary" :to="`/product/${notif.data.box_id}`">{{ genesisStore.metadata[notif.data.box_id]?._data?.name ?? notif.data.box_id }}</RouterLink></p>
         </template>
         <p v-show="!toast" class="text-xs text-grad-dark mt-2 flex justify-between">
             <span>{{ new Date(notif.timestamp).toLocaleString("en-uk", { dateStyle: "long", timeStyle: "short" }) }}</span>
