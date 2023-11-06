be71fa125eb933289cac929b675bf1e8d3fbefe6
Lancelot de Ferrière <wraitii@gmail.com>
2023-11-06 17:48:02 +0100

    De-activate auto-briq-migration for now

--- src/chain/contracts/migration.ts
+++ src/chain/contracts/migration.ts
@@ -6,10 +6,12 @@ import type { Call } from 'starknet';
 export function migrateBriqsIfNeeded(calls: Call[]) {
     if (!legacyChainBriqs.current?.getNbBriqs())
         return calls;
+    /*
     calls.unshift({
         contractAddress: ADDRESSES[getCurrentNetwork()].migrate_assets,
         entrypoint: 'migrate_legacy_briqs',
         calldata: [legacyChainBriqs.current!.getNbBriqs()],
     });
+    */
     return calls;
 }
