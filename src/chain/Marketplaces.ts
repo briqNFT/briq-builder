e77aa2e7b6b26371f38a0a4266a7f6add23d0b6c
Lancelot de Ferrière <wraitii@gmail.com>
2023-12-04 10:13:59 +0100

    Fix flex links as well

--- src/chain/Marketplaces.ts
+++ src/chain/Marketplaces.ts
@@ -8,8 +8,8 @@ export function getSetLink(marketplace: string, network: CHAIN_NETWORKS, collect
         return `https://unframed.co/item/${getSetAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'element') // They need 64-item long hex string
         return `https://element.market/assets/starknet/0x${getSetAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
-    if (marketplace === 'flex')
-        return `https://flexing.gg/starknet/asset/${getSetAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
+    if (marketplace === 'flex') // They need 64-item long hex string
+        return `https://flexing.gg/starknet/asset/0x${getSetAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'pyramid') // they don't support leading 0
         return `https://pyramid.market/asset/0x${BigInt(getSetAddress(ADDRESSES[network], collection)).toString(16)}/${BigInt(setId).toString(10)}`;
 }
@@ -20,8 +20,8 @@ export function getBookletLink(marketplace: string, network: CHAIN_NETWORKS, col
         return `https://unframed.co/item/${getBookletAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'element') // They need 64-item long hex string
         return `https://element.market/assets/starknet/0x${getBookletAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
-    if (marketplace === 'flex')
-        return `https://flexing.gg/starknet/asset/${getBookletAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
+    if (marketplace === 'flex') // They need 64-item long hex string
+        return `https://flexing.gg/starknet/asset/0x${getBookletAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'pyramid') // they don't support leading 0
         return `https://pyramid.market/asset/0x${BigInt(getBookletAddress(ADDRESSES[network], collection)).toString(16)}/${BigInt(setId).toString(10)}`;
 }
@@ -32,8 +32,8 @@ export function getBoxLink(marketplace: string, network: CHAIN_NETWORKS, collect
         return `https://unframed.co/item/${getBoxAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'element') // They need 64-item long hex string
         return `https://element.market/assets/starknet/0x${getBoxAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
-    if (marketplace === 'flex')
-        return `https://flexing.gg/starknet/asset/${getBoxAddress(ADDRESSES[network], collection)}/${BigInt(setId).toString(10)}`;
+    if (marketplace === 'flex') // They need 64-item long hex string
+        return `https://flexing.gg/starknet/asset/0x${getBoxAddress(ADDRESSES[network], collection).slice(2).padStart(64, '0')}/${BigInt(setId).toString(10)}`;
     if (marketplace === 'pyramid') // they don't support leading 0
         return `https://pyramid.market/asset/0x${BigInt(getBoxAddress(ADDRESSES[network], collection)).toString(16)}/${BigInt(setId).toString(10)}`;
 }
